648204bdb56ffacf4f8a0df8622440b1b51ba33b
Anastasia <dbwjddbsgk@gmail.com>
2021-04-13 12:05:56 +0900

    버튼 추가 및 클릭 이벤트 핸들러 구현

--- HelloUWP/Views/MainPage.xaml.cs
+++ HelloUWP/Views/MainPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 
 namespace HelloUWP.Views
@@ -27,5 +27,14 @@ namespace HelloUWP.Views
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var myDlg = new MessageDialog("안녕하세요");
+            myDlg.Commands.Add(new UICommand("OK"));
+            myDlg.Commands.Add(new UICommand("아니오"));
+            myDlg.Commands.Add(new UICommand("Bye"));
+            await myDlg.ShowAsync();
+        }
     }
 }
